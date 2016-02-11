68493ff78f3c1204565f6c3ebe3b221837d43fa8
Nuno Coelho Santos <nuno@nunocoelhosantos.com>
2016-02-11 19:22:44 +0900

    Interface layout

---
+++ Currency/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Currency
+//
+//  Created by Nuno Coelho Santos on 11/02/2016.
+//  Copyright © 2016 Nuno Coelho Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
