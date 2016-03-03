daca0502710fd634e2a9ff93b5b303f91a14d08a
Nuno Coelho Santos <nuno@nunocoelhosantos.com>
2016-03-03 11:29:44 +0900

    Add comments to the ApplDelegate file on the database clone implementation

--- Currency/AppDelegate.swift
+++ Currency/AppDelegate.swift
@@ -10,6 +10,7 @@ import UIKit
 import Realm
 import RealmSwift
 
+// Define a function to get a path of a bundled asset by passing the asset name.
 func bundlePath(path: String) -> String? {
     let resourcePath = NSBundle.mainBundle().resourcePath as NSString?
     return resourcePath?.stringByAppendingPathComponent(path)
@@ -22,10 +23,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
+        // First, check that the database is empty.
         if Currency.allObjects().count == 0 {
+            // Then, define the path for the default realm database as a constant.
             let defaultPath = Realm.Configuration.defaultConfiguration.path!
+            // If we can find the initial database dump on the bundle...
             if let initialDatabasePath = bundlePath("initial.realm") {
                 do {
+                    // Remove the database on the default path and place the bundled database there instead.
                     try NSFileManager.defaultManager().removeItemAtPath(defaultPath)
                     try NSFileManager.defaultManager().copyItemAtPath(initialDatabasePath, toPath: defaultPath)
                 } catch {}
