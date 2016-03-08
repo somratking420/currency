ca6a3ab3f7b5d7cda3c529a59c23a6d93b19bedf
Nuno Coelho Santos <nuno@nunocoelhosantos.com>
2016-03-08 23:25:24 +0900

    Correct number formatting

--- Currency/Classes/Converter.swift
+++ Currency/Classes/Converter.swift
@@ -13,29 +13,34 @@ import SWXMLHash
 class Converter {
 
     var input: String
-    var inputCurrencyCode: String
-    var outputCurrencyCode: String
-    var inputCurrencyExchangeRate: Double
-    var outputCurrencyExchangeRate: Double
+    var inputCurrency:(code: String, rate: Double, locale: String?, symbol: String?)
+    var outputCurrency:(code: String, rate: Double, locale: String?, symbol: String?)
     
     init() {
         input = "0"
-        inputCurrencyCode = "JPY";
-        outputCurrencyCode = "GBP";
-        inputCurrencyExchangeRate = 113.81;
-        outputCurrencyExchangeRate = 0.71;
-        requestUpdateForCurrencyConvertionRate(inputCurrencyCode)
-        requestUpdateForCurrencyConvertionRate(outputCurrencyCode)
+        
+        inputCurrency.code = "JPY";
+        inputCurrency.rate = 113.81;
+        inputCurrency.locale = "ja_JP"
+        inputCurrency.symbol = "¥"
+        
+        outputCurrency.code = "GBP";
+        outputCurrency.rate = 0.71;
+        outputCurrency.locale = "en_GB"
+        outputCurrency.symbol = "£"
+        
+        requestUpdateForCurrencyExchangeRate(inputCurrency.code)
+        requestUpdateForCurrencyExchangeRate(outputCurrency.code)
     }
 
     func inputValue() -> String {
         let inputValue: Double = Double(input)!
-        return convertToCurrency(inputValue, currencyCode: inputCurrencyCode)
+        return convertToCurrency(inputValue, code: inputCurrency.code, locale: inputCurrency.locale, symbol: inputCurrency.symbol)
     }
 
     func outputValue() -> String {
-        let outputValue: Double = (Double(input)! / inputCurrencyExchangeRate) * outputCurrencyExchangeRate
-        return convertToCurrency(outputValue, currencyCode: outputCurrencyCode)
+        let outputValue: Double = (Double(input)! / inputCurrency.rate) * outputCurrency.rate
+        return convertToCurrency(outputValue, code: outputCurrency.code, locale: outputCurrency.locale, symbol: outputCurrency.symbol)
     }
 
     func addInput(string: String) {
@@ -51,14 +56,20 @@ class Converter {
     }
 
     func setInputCurrency(currencyCode: String) {
-        inputCurrencyCode = currencyCode
-        requestUpdateForCurrencyConvertionRate(inputCurrencyCode)
+        let currency = getCurrencyRecord(currencyCode)
+        inputCurrency.code = currency.code
+        inputCurrency.locale = currency.locale
+        inputCurrency.symbol = currency.symbol
+        requestUpdateForCurrencyExchangeRate(currency.code)
         print("Set input currency to: \(currencyCode).")
     }
 
     func setOutputCurrency(currencyCode: String) {
-        outputCurrencyCode = currencyCode
-        requestUpdateForCurrencyConvertionRate(outputCurrencyCode)
+        let currency = getCurrencyRecord(currencyCode)
+        outputCurrency.code = currency.code
+        outputCurrency.locale = currency.locale
+        outputCurrency.symbol = currency.symbol
+        requestUpdateForCurrencyExchangeRate(currency.code)
         print("Set output currency to: \(currencyCode).")
     }
 
@@ -70,17 +81,26 @@ class Converter {
         input = "0";
     }
 
-    private func convertToCurrency(value: Double, currencyCode: String) -> String {
+    private func convertToCurrency(value: Double, code: String, locale: String?, symbol: String?) -> String {
         let formatter = NSNumberFormatter()
         formatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
-        formatter.currencyCode = currencyCode
+        
+        if let locale = locale where !locale.isEmpty {
+            formatter.locale = NSLocale(localeIdentifier: locale)
+        } else if let symbol = symbol where !symbol.isEmpty {
+            formatter.positivePrefix = symbol
+            formatter.negativePrefix = symbol
+        } else {
+            formatter.currencySymbol = ""
+        }
+        
         formatter.usesGroupingSeparator = true;
         formatter.groupingSeparator = ","
         let formattedPriceString = formatter.stringFromNumber(value)
         return formattedPriceString!
     }
 
-    private func requestUpdateForCurrencyConvertionRate(currencyCode: String) {
+    private func requestUpdateForCurrencyExchangeRate(currencyCode: String) {
         
         let url = NSURL(string: "https://query.yahooapis.com/v1/public/yql?q=" +
             "select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(" +
@@ -101,15 +121,15 @@ class Converter {
             
             // If we are dealing with the currency input currency,
             // let's update the current input rate.
-            if currencyCode == self.inputCurrencyCode {
-                self.inputCurrencyExchangeRate = Double(rate)!
+            if currencyCode == self.inputCurrency.code {
+                self.inputCurrency.rate = Double(rate)!
                 print("Input currency updated.")
             }
             
             // If we are dealing with the currency output currency,
             // let's update the current output rate.
-            if currencyCode == self.outputCurrencyCode {
-                self.outputCurrencyExchangeRate = Double(rate)!
+            if currencyCode == self.outputCurrency.code {
+                self.outputCurrency.rate = Double(rate)!
                 print("Output currency updated.")
             }
             
@@ -151,6 +171,32 @@ class Converter {
         
         print("Currency \(currencyCode) updated with the rate: \(rate)")
         
+    }
+    
+    private func getCurrencyRecord(currencyCode: String) -> (name: String, code: String, rate: Double, locale: String?, symbol: String?)  {
+        
+        // CoreData setup.
+        let managedObjectContext: NSManagedObjectContext!
+        let appDelegate: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        managedObjectContext = appDelegate.managedObjectContext as NSManagedObjectContext
+        var currency: Currency
+        
+        // CoreData fetching.
+        let fetch = NSFetchRequest(entityName: "Currency")
+        let predicate = NSPredicate(format: "%K == %@", "code", currencyCode)
+        fetch.predicate = predicate
+        fetch.fetchLimit = 1
+        
+        do {
+            currency = try managedObjectContext.executeFetchRequest(fetch).first as! Currency
+        } catch {
+            fatalError("Error fetching currency: \(error)")
+        }
+        
+        let rate:Double! = Double(currency.rateFromUSD!)
+        
+        return(currency.name!, currencyCode, rate, currency.locale, currency.symbol)
+    
     }
 
 }
