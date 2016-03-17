a715512e8e2ff9edad9ff06dd1ca99a634fe6fb7
Nuno Coelho Santos <nuno@nunocoelhosantos.com>
2016-03-18 01:49:14 +0900

    Shorter row height

--- Currency/ChangeCurrencyViewController.swift
+++ Currency/ChangeCurrencyViewController.swift
@@ -34,7 +34,7 @@ class ChangeCurrencyViewController: UIViewController {
 
         self.tableView.delegate = self
         self.tableView.dataSource = self
-        self.tableView.rowHeight = 64.0
+        self.tableView.rowHeight = 54.0
         let appDelegate: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         managedObjectContext = appDelegate.managedObjectContext as NSManagedObjectContext
 
