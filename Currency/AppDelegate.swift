024003f5e8c268ee1a7a69eabd7b70f9fb285986
Nuno Coelho Santos <nuno@nunocoelhosantos.com>
2016-03-27 23:37:13 +0900

    Revert "Replace C-style for loop"
    This reverts commit bb9866d129e41a08913f1f7422128e0a689cb122.

--- Currency/AppDelegate.swift
+++ Currency/AppDelegate.swift
@@ -68,7 +68,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             let sourceSqliteURLs = [NSBundle.mainBundle().URLForResource("InitialCurrencyDatabase", withExtension: "sqlite")!, NSBundle.mainBundle().URLForResource("InitialCurrencyDatabase", withExtension: "sqlite-wal")!, NSBundle.mainBundle().URLForResource("InitialCurrencyDatabase", withExtension: "sqlite-shm")!]
             let destSqliteURLs = [self.applicationDocumentsDirectory.URLByAppendingPathComponent("CurrencyDatabase.sqlite"), self.applicationDocumentsDirectory.URLByAppendingPathComponent("CurrencyDatabase.sqlite-wal"), self.applicationDocumentsDirectory.URLByAppendingPathComponent("CurrencyDatabase.sqlite-shm")]
             
-            for index in 1...sourceSqliteURLs.count {
+            for var index = 0; index < sourceSqliteURLs.count; index++ {
                 do {
                     try NSFileManager.defaultManager().copyItemAtURL(sourceSqliteURLs[index], toURL: destSqliteURLs[index])
                 } catch {
