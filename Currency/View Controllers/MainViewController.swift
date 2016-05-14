5e977b8191276fdd104272fc502e543ca383e98c
Nuno Coelho Santos <nuno@nunocoelhosantos.com>
2016-05-14 11:31:37 +0900

    Fix issue with Norwegian Kroner

--- Currency/View Controllers/MainViewController.swift
+++ Currency/View Controllers/MainViewController.swift
@@ -33,6 +33,7 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         converter = Converter()
         calculator = Calculator()
         
