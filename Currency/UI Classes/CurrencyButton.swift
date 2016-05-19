d25dd28e7645fc8c0199435306fcba1c9848dccb
Nuno Coelho Santos <nuno@nunocoelhosantos.com>
2016-05-19 11:17:19 +0900

    Fade currency buttons on tap, like standard UI Buttons

---
+++ Currency/UI Classes/CurrencyButton.swift
@@ -0,0 +1,49 @@
+//
+//  CurrencyButton.swift
+//  Currency
+//
+//  Created by Nuno Coelho Santos on 19/05/2016.
+//  Copyright © 2016 Nuno Coelho Santos. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CurrencyButton: UIButton {
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+    }
+    
+    override var highlighted: Bool {
+        get {
+            return super.highlighted
+        }
+        set {
+            if newValue {
+                let fadeOut = CABasicAnimation(keyPath: "opacity")
+                fadeOut.fromValue = 1
+                fadeOut.toValue = 0.3
+                fadeOut.duration = 0.03
+                fadeOut.autoreverses = false
+                fadeOut.repeatCount = 1
+                
+                self.layer.addAnimation(fadeOut, forKey: "fadeOut")
+                self.layer.opacity = 0.3
+            }
+            else {
+                let fadeIn = CABasicAnimation(keyPath: "opacity")
+                fadeIn.fromValue = 0.3
+                fadeIn.toValue = 1
+                fadeIn.duration = 0.12
+                fadeIn.autoreverses = false
+                fadeIn.repeatCount = 1
+                
+                self.layer.addAnimation(fadeIn, forKey: "fadeIn")
+                self.layer.opacity = 1
+            }
+            super.highlighted = newValue
+        }
+    }
+    
+}
