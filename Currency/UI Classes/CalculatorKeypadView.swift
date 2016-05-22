19072903d811bfc54c403f0ea2472edaf43f1045
Nuno Coelho Santos <nuno@nunocoelhosantos.com>
2016-05-22 13:09:01 +0900

    Add a thin border on top of the keyboard show all borders show the same thickness on the 6plus size

--- Currency/UI Classes/CalculatorKeypadView.swift
+++ Currency/UI Classes/CalculatorKeypadView.swift
@@ -11,4 +11,16 @@ import UIKit
 
 class CalculatorKeypadView: UIView {
     
+    let borderColor: CGColor! = UIColor(red:0.85, green:0.85, blue:0.85, alpha:1.00).CGColor
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        
+        let border = CALayer()
+        border.backgroundColor = borderColor
+        border.frame = CGRect(x: 0, y: -0.25, width: self.frame.size.width * 2, height: 0.25)
+        self.layer.addSublayer(border)
+        self.layer.masksToBounds = false
+    }
+    
 }
